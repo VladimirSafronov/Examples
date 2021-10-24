c9827ca0f0d2fbf3679453379117ff04181ed3c0
VladimirSafronov <safron5000@mail.ru>
2021-10-24 18:08:30 +0300

    Changes Example 004 div

--- Example004_Div/Program.cs
+++ Example004_Div/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Программа частного двух чисел
+double numberA = 24;
+double numberB = 5;
+Console.WriteLine(numberA / numberB);
