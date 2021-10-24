5464c09ed9a1e392d1e8776049fd9413f1b43ad7
VladimirSafronov <safron5000@mail.ru>
2021-10-24 15:50:18 +0300

    Add random sum

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,5 +1,7 @@
 ﻿// Программа сложения двух чисел
-int numberA = 3;
-int numberB = 5;
+int numberA = new Random().Next(1, 10); // числа от 1 до 9
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
 int result = numberA + numberB;
 Console.WriteLine(result);
