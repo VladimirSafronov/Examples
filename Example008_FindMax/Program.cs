b0cfbf11990b6d4932a11b359e4b1ba2cfb14b4a
VladimirSafronov <safron5000@mail.ru>
2021-10-27 14:04:29 +0300

    Find Max

---
+++ Example008_FindMax/Program.cs
@@ -0,0 +1,39 @@
+﻿// поиск максимального значения
+int a1 = 21;
+int b1 = 32212;
+int c1 = 2;
+
+int a2 = 42;
+int b2 = 222;
+int c2 = 54;
+
+int a3 = 7;
+int b3 = 898;
+int c3 = 5;
+
+//int max = a1;
+//if (b1 > max) max = b1;
+//if (c1 > max) max = c1;
+//if (a2 > max) max = a2;
+//if (b2 > max) max = b2;
+//if (c2 > max) max = c2;
+//if (a3 > max) max = a3;
+//if (b3 > max) max = b3;
+//if (c3 > max) max = c3;  - стихийный метод
+
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}                        
+
+// int max1 = Max(a1, b1, c1);
+// int max2 = Max(a2, b2, c2);
+// int max3 = Max(a3, b3, c3);
+// int max = Max(max1, max2, max3);  - функция
+
+int max = Max(Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3)); // функциональное программирование
+
+Console.WriteLine($"max = {max}");
