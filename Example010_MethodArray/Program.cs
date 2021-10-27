3e36438b7eba43d4add6e4fa5354a382c62b61f5
VladimirSafronov <safron5000@mail.ru>
2021-10-27 15:06:40 +0300

    Method Array

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,15 @@
+﻿// поиск элемента массива с определенным значением
+int[] array = {71,22,3,34,51,68,67,48,93};
+int n = array.Length;
+int find = 48;
+int index = 0;
+
+while(index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break; // - прервет выполнение при нахождении первого элемента с заданным значением
+    }
+    index++;
+}
